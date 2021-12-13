f14a3ee1bf57d2d90cf148525d1613de29f9ebb5
Harry Jones <harryrjones@yahoo.com>
2021-12-13 12:25:43 +0000

    Added a Class to write to the command line. Currently asks for an algorithm and a size for an array in separate methods.

---
+++ src/main/java/sort_manager/io_interface/Writer.java
@@ -0,0 +1,12 @@
+package sort_manager.io_interface;
+
+public class Writer {
+    public void askAlgorithm(){
+        System.out.println("Which Algorithm would you like to use?");
+
+    }
+
+    public void askNumber(){
+        System.out.println("Enter how many numbers you would like in the array");
+    }
+}
