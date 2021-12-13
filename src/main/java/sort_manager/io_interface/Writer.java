f1b637443bb156f98e2032351ddb3de04a6081a0
Harry Jones <harryrjones@yahoo.com>
2021-12-13 15:29:16 +0000

    Added the list of algorithms to the output

--- src/main/java/sort_manager/io_interface/Writer.java
+++ src/main/java/sort_manager/io_interface/Writer.java
@@ -1,12 +1,22 @@
 package sort_manager.io_interface;
 
 public class Writer {
+    /**
+     * Method to ask the user to pick a sorting algorithm
+     */
     public void askAlgorithm(){
         System.out.println("Which Algorithm would you like to use?");
+        System.out.println("The options are: ");
+        System.out.println("Bubble");
+        System.out.println("Binary");
+        System.out.println("Merge");
 
     }
 
+    /**
+     * Method to ask the user to input an integer
+     */
     public void askNumber(){
-        System.out.println("Enter how many numbers you would like in the array");
+        System.out.println("Enter how many numbers you would like in the array. Type your answer as a numerical whole number (E.G 400)");
     }
 }
