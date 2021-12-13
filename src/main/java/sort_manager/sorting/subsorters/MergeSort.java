a00d41a83f531d62e03ccf65392b35e89e8449eb
Harry Jones <harryrjones@yahoo.com>
2021-12-13 15:56:05 +0000

    Added mergesort using java api Arrays.sort()

--- src/main/java/sort_manager/sorting/subsorters/MergeSort.java
+++ src/main/java/sort_manager/sorting/subsorters/MergeSort.java
@@ -2,10 +2,18 @@ package sort_manager.sorting.subsorters;
 
 import sort_manager.sorting.Sorter;
 
+import java.util.Arrays;
+
 public class MergeSort extends Sorter {
 
-
+    /**
+     * Method to sort using the inbuilt mergeSort algorithm
+     * @param unsortedArray the array to be sorted
+     * @return int[] the sorted array
+     */
     public int[] sort(int[] unsortedArray) {
-        return new int[0];
+
+        Arrays.sort(unsortedArray);
+        return unsortedArray;
     }
 }
