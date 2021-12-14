a4cfad4d30ebc3643d4dda40d21bf0979ddaafa5
Harry Jones <harryrjones@yahoo.com>
2021-12-14 16:44:01 +0000

    Nested Node class within BinaryTree; It is only used here.

--- src/main/java/sort_manager/sorting/subsorters/binarysort/BinaryTree.java
+++ src/main/java/sort_manager/sorting/subsorters/binarysort/BinaryTree.java
@@ -4,6 +4,46 @@ package sort_manager.sorting.subsorters.binarysort;
  * Class to represent a BinaryTree of Nodes
  */
 public class BinaryTree implements BinaryTreeI{
+
+    //Nested Class represents the nodes within a BinaryTree
+    public class Node {
+        private final int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(Node leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public Node getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(Node rightChild) {
+            this.rightChild = rightChild;
+        }
+
+        public boolean isLeftChildEmpty(){
+            return leftChild == null;
+        }
+
+        public boolean isRightChildEmpty(){
+            return rightChild == null;
+        }
+    }
+
     private final Node rootNode;
 
     /**
