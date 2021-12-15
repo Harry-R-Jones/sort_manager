d9df918210562a0a78bcd546fea90b98cd50686a
Harry Jones <harryrjones@yahoo.com>
2021-12-15 16:17:37 +0000

    extracted some methods and added variables to store relevant information

--- src/main/java/sort_manager/ProgramManager.java
+++ src/main/java/sort_manager/ProgramManager.java
@@ -5,21 +5,47 @@ import sort_manager.sorting.SortManager;
 
 public class ProgramManager {
     IOManager ioManager;
+    SortManager sortManager;
+    int[] unsortedArray;
 
     public ProgramManager(){
         ioManager = new IOManager();
+        sortManager = createSortManager();
     }
 
     public void run(){
-        String algorithm;
+        ioManager.writeArray(getUnsortedArray());
 
+        sortManager.timedSort();
+
+        ioManager.printOutputs(sortManager.getSortedArray(), sortManager.getAlgorithm(), sortManager.getTime());
+    }
+
+    private SortManager createSortManager() {
+        String algorithm = getAlgorithm();
+        int arraySize = ioManager.getNumber();
+
+        setUnsortedArray(RandomArrayGenerator.generate(arraySize));
+
+        sortManager = new SortManager(algorithm, unsortedArray);
+
+        return sortManager;
+    }
+
+    private void setUnsortedArray(int[] generatedArray) {
+        unsortedArray = generatedArray;
+    }
+
+    private int[] getUnsortedArray(){
+        return unsortedArray;
+    }
+
+    private String getAlgorithm() {
+        String algorithm;
         do {
             algorithm = ioManager.getAlgorithm();
         } while(algorithm.equals("Invalid"));
 
-        int arraySize = ioManager.getNumber();
-
-        SortManager sortManager = new SortManager(algorithm, RandomArrayGenerator.generate(arraySize));
-        sortManager.timedSort();
+        return algorithm;
     }
 }
