e67ecdbd2c1c074f7c7eb75fe6eb3c4f72a7e5d0
Harry Jones <harryrjones@yahoo.com>
2021-12-15 11:20:21 +0000

    Added basic sorting tests to the sorting classes

---
+++ src/test/java/sort_manager/sorting/subsorters/BubbleSortTest.java
@@ -0,0 +1,32 @@
+package sort_manager.sorting.subsorters;
+
+import org.junit.jupiter.api.*;
+import sort_manager.sorting.subsorters.binarysort.MergeSort;
+
+public class BubbleSortTest {
+    int[] unsorted;
+    int[] sorted;
+    MergeSort bubbleSort;
+
+    @BeforeAll
+    static void setupAll(TestInfo testinfo){
+        System.out.println(testinfo.getTestClass() + " executing");
+    }
+
+    @BeforeEach
+//    @ValueSource(ints = {12, 78, 21, -1000, 9})
+    void setup(TestInfo testInfo) {
+        unsorted = new int[]{12, 78, 21, -1000, 9};
+        sorted= new int[]{-1000, 9, 12, 21, 78};
+
+        bubbleSort = new MergeSort(unsorted);
+        System.out.println(testInfo.getDisplayName() + " - is being tested");
+
+    }
+
+    @Test
+    @DisplayName("Test array gets sorted")
+    void testArrayGetsSorted() {
+        Assertions.assertArrayEquals(sorted, bubbleSort.sort());
+    }
+}
