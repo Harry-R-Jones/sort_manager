3482aa30ce88a20ded5538bf3e54e2d94bc191f4
Harry Jones <harryrjones@yahoo.com>
2021-12-15 16:20:08 +0000

    Explained the usage of the print methods

--- src/main/java/sort_manager/io_interface/Writer.java
+++ src/main/java/sort_manager/io_interface/Writer.java
@@ -12,7 +12,6 @@ public class Writer {
         System.out.println("Bubble");
         System.out.println("Binary");
         System.out.println("Merge");
-
     }
 
     /**
@@ -22,10 +21,19 @@ public class Writer {
         System.out.println("Enter how many numbers you would like in the array. Type your answer as a numerical whole number (E.G 400)");
     }
 
+    /**
+     * Method to print an array in the format [n, n + 1, ....]
+     * @param array the array to print
+     */
     public void printArray(int[] array) {
         System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * Method to print a description of what we've done to the array and how long it took.
+     * @param algorithm the algorithm we used
+     * @param time the time taken in nanoseconds
+     */
     public void printDescription(String algorithm, long time) {
         System.out.println("Was sorted using " + algorithm + " in " + time + " nanoseconds to give: ");
     }
