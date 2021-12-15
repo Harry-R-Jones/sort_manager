de6248abd791e1534b7212d7746e9f6601a5c663
Harry Jones <harryrjones@yahoo.com>
2021-12-15 16:16:48 +0000

    Added some getters and setters to access the time and the original array so that we can compare the two

--- src/main/java/sort_manager/sorting/SortManager.java
+++ src/main/java/sort_manager/sorting/SortManager.java
@@ -3,33 +3,46 @@ package sort_manager.sorting;
 public class SortManager {
     private String algorithm;
     private int[] unsortedArray;
+    private int[] sortedArray;
     private long timeInNanoSecs;
     private Sorter sorter;
 
     public SortManager(String algorithm, int[] unsortedArray) {
         this.algorithm = algorithm;
         this.unsortedArray = unsortedArray;
+        this.sortedArray = null;
         sorter = SorterFactory.getSorter(algorithm, unsortedArray);
 
     }
 
-    public int[] timedSort(){
+    public void timedSort(){
 
         Long startTime = System.nanoTime();
 
-        int[] sortedArray = sort(unsortedArray, algorithm);
+        setSortedArray(sorter.sort());
 
         Long endTime = System.nanoTime();
 
         timeInNanoSecs = endTime - startTime;
+    }
 
+    public long getTime() {
+        return timeInNanoSecs;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public int[] getUnsortedArray() {
+        return unsortedArray;
+    }
+
+    public int[] getSortedArray() {
         return sortedArray;
     }
 
-    public int[] sort(int[] unsortedArray, String algorithm){
-        int[] sortedArray = new int[unsortedArray.length];
-
-
-    return null;
+    public void setSortedArray(int[] sortedArray) {
+        this.sortedArray = sortedArray;
     }
 }
