dc24796d99c0f716dc7f7acba6c34476040fecdc
Harry Jones <harryrjones@yahoo.com>
2021-12-16 17:06:29 +0000

    Added log message that program is starting

--- src/main/java/sort_manager/App.java
+++ src/main/java/sort_manager/App.java
@@ -2,8 +2,14 @@ package sort_manager;
 
 import sort_manager.io_interface.Reader;
 
+import java.util.logging.Level;
+
 public class App {
     public static void main(String[] args) {
+
+        MyLogger.setLogLevel(Level.ALL);
+        MyLogger.log(Level.INFO, "Program Started");
+
         //Start the program
         ProgramManager programManager = new ProgramManager();
         programManager.run();
